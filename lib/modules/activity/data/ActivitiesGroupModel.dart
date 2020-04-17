12192eabea62a3b06b6cb5bc8a4a4bf34b438258
Vladimir Berdnik <user.boba@gmail.com>
2020-04-17 15:56:38 +0600

    Add basic main models of new project

---
+++ lib/modules/activity/data/ActivitiesGroupModel.dart
@@ -0,0 +1,45 @@
+import 'package:timertinio/modules/activity/data/ActivityModel.dart';
+
+class ActivitiesGroup {
+  List<Activity> _items = [];
+
+  ActivitiesGroup();
+
+  void add(Activity activity) {
+    this._items.add(activity);
+  }
+
+  Activity getActivity(Activity activity) {
+    return _items.firstWhere((Activity item) => item.name == activity.name);
+  }
+
+  void startActivity(Activity activity) {
+    getActivity(activity).start();
+  }
+
+  void stopActivity(Activity activity) {
+    getActivity(activity).stop();
+  }
+
+  void resetActivity(Activity activity) {
+    getActivity(activity).reset();
+  }
+
+  toJson() {
+    List serializedItems = [];
+    _items.forEach((Activity activity) {
+      serializedItems.add(activity.toJson());
+    });
+
+    return {'groupItems': serializedItems};
+  }
+
+  static ActivitiesGroup fromJson(json) {
+    ActivitiesGroup activitiesGroup = ActivitiesGroup();
+    (json['groupItems'] as List).forEach((value) {
+      activitiesGroup.add(Activity.fromJson(value));
+    });
+
+    return activitiesGroup;
+  }
+}
---
+++ lib/modules/activity/data/ActivityModel.dart
@@ -0,0 +1,48 @@
+class Activity {
+  final String _name;
+  double _duration = 0.0;
+  DateTime _lastStartTime;
+
+  Activity(this._name);
+
+  String get name => _name;
+
+  bool isStarted() {
+    return _lastStartTime != null;
+  }
+
+  void start() {
+    if (isStarted()) {
+      return;
+    }
+
+    _lastStartTime = DateTime.now();
+  }
+
+  void stop() {
+    if (!isStarted()) {
+      return;
+    }
+
+    _duration += DateTime.now().difference(_lastStartTime).inSeconds;
+  }
+
+  void reset() {
+    this._duration = 0.0;
+    this._lastStartTime = null;
+  }
+
+  toJson() {
+    return {
+      'name': _name,
+      'duration': _duration,
+      'lastStartTime': _lastStartTime.millisecondsSinceEpoch,
+    };
+  }
+
+  static Activity fromJson(json) {
+    Activity activity = Activity(json['name']);
+    activity._duration = json['duration'] as double;
+    activity._lastStartTime = DateTime.fromMillisecondsSinceEpoch(json['lastStartTime'] as int);
+  }
+}
