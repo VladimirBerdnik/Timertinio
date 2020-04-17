c79a206af5c48cd57eb1a22a2430c1d348b2d4e2
Vladimir Berdnik <user.boba@gmail.com>
2020-04-17 20:09:47 +0600

    Add dummy activity

--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,13 +1,19 @@
 import 'dart:convert';
 
 import 'package:timertinio/modules/activity/data/ActivitiesGroupModel.dart';
+import 'package:timertinio/modules/activity/data/ActivityModel.dart';
 
 class AppState {
   final ActivitiesGroup activitiesGroup;
 
   AppState(this.activitiesGroup);
 
-  AppState.emptyState() : activitiesGroup = ActivitiesGroup();
+  static AppState emptyState() {
+    ActivitiesGroup activitiesGroup = ActivitiesGroup();
+    activitiesGroup.add(Activity('Разработка приложения'));
+
+    return AppState(activitiesGroup);
+  }
 
   static AppState fromJson(dynamic json) => AppState(ActivitiesGroup.fromJson(jsonDecode(json['activitiesGroup'])));
 
