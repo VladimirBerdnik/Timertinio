842ff6988af0ed062bd83be1f08be128c13ace2f
Vladimir Berdnik <user.boba@gmail.com>
2020-04-18 13:17:40 +0600

    Add ability to pick color for Activity

--- lib/modules/activity/widgets/activities-group-page.dart
+++ lib/modules/activity/widgets/activities-group-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:timertinio/modules/activity/data/ActivitiesGroupModel.dart';
 import 'package:timertinio/modules/activity/data/ActivityModel.dart';
@@ -6,34 +7,92 @@ import 'package:timertinio/modules/activity/widgets/activity-card.dart';
 import 'package:timertinio/state/actions.dart';
 import 'package:timertinio/state/state.dart';
 
-class ActivitiesGroupPage extends StatelessWidget {
+class ActivitiesGroupPage extends StatefulWidget {
+  @override
+  _ActivitiesGroupPageState createState() => _ActivitiesGroupPageState();
+}
+
+class _ActivitiesGroupPageState extends State<ActivitiesGroupPage> {
   @override
   Widget build(BuildContext context) {
     TextEditingController _textFieldController = TextEditingController();
+    Color _tempColor;
+    Color _newActivityColor = Colors.blue[200];
+    String _newActivityName;
+
+    _openColorPicker() async {
+      return showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            contentPadding: const EdgeInsets.all(6.0),
+            title: Text("Pick the Activity color"),
+            content: MaterialColorPicker(
+              allowShades: false,
+              colors: fullMaterialColors,
+              selectedColor: _newActivityColor,
+              onMainColorChange: (color) => setState(() => _tempColor = color),
+            ),
+            actions: [
+              FlatButton(
+                child: Text('CANCEL'),
+                onPressed: Navigator.of(context).pop,
+              ),
+              FlatButton(
+                child: Text('SUBMIT'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  setState(() {
+                    _newActivityColor = _tempColor;
+                  });
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
 
     _displayDialog(BuildContext context) async {
-      _textFieldController.text = '';
-
       return showDialog(
           context: context,
           builder: (context) {
+            _textFieldController.text = '';
+
             return AlertDialog(
-              title: Text('Enter new Activity details'),
-              content: TextField(
-                controller: _textFieldController,
-                decoration: InputDecoration(hintText: 'Activity name'),
-              ),
+              title: Text('New Activity details'),
+              content: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: _textFieldController,
+                        decoration: InputDecoration(hintText: 'Activity name'),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: _openColorPicker,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 16.0,
+                        child: Icon(Icons.color_lens, color: Colors.black),
+                      ),
+                    )
+                  ]),
               actions: <Widget>[
                 FlatButton(
-                  child: new Text('OK'),
-                  onPressed: () {
-                    Navigator.of(context).pop(_textFieldController.text);
-                  },
+                  child: new Text('CANCEL'),
+                  onPressed: Navigator.of(context).pop,
                 ),
                 FlatButton(
-                  child: new Text('CANCEL'),
+                  child: new Text('SUBMIT'),
                   onPressed: () {
-                    Navigator.of(context).pop(null);
+                    Navigator.of(context).pop(true);
+                    setState(() {
+                      _newActivityName = _textFieldController.text;
+                    });
                   },
                 ),
               ],
@@ -53,10 +112,12 @@ class ActivitiesGroupPage extends StatelessWidget {
                   builder: (context, ActivitiesGroup activitiesGroup) {
                     return FloatingActionButton(
                         onPressed: () async {
-                          String result = await _displayDialog(context);
+                          if (!await _displayDialog(context)) {
+                            return;
+                          }
 
-                          if (result != null) {
-                            store.dispatch(AddActivityAction(result));
+                          if (_newActivityName != null) {
+                            store.dispatch(AddActivityAction(_newActivityName, _newActivityColor));
                           }
                         },
                         child: Icon(Icons.alarm_add));
