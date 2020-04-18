2d577496acb22f17a1dc1e18ea1c4d79e534a8fa
Vladimir Berdnik <user.boba@gmail.com>
2020-04-18 19:10:14 +0600

    Extract activity dialog into separate component

--- lib/modules/activity/widgets/activities-group-page.dart
+++ lib/modules/activity/widgets/activities-group-page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:timertinio/modules/activity/data/ActivitiesGroupModel.dart';
 import 'package:timertinio/modules/activity/data/ActivityModel.dart';
 import 'package:timertinio/modules/activity/widgets/activity-card.dart';
+import 'package:timertinio/modules/activity/widgets/activity-dialog.dart';
 import 'package:timertinio/state/actions.dart';
 import 'package:timertinio/state/state.dart';
 
@@ -15,100 +15,25 @@ class ActivitiesGroupPage extends StatefulWidget {
 class _ActivitiesGroupPageState extends State<ActivitiesGroupPage> {
   @override
   Widget build(BuildContext context) {
-    TextEditingController _textFieldController = TextEditingController();
-    Color _tempColor;
-    Color _newActivityColor = Colors.blue[100];
-    String _newActivityName;
-
-    _openColorPicker() async {
-      return showDialog(
-        context: context,
-        builder: (_) {
-          return AlertDialog(
-            contentPadding: const EdgeInsets.all(6.0),
-            title: Text("Pick the Activity color"),
-            content: MaterialColorPicker(
-              colors: materialColors,
-              selectedColor: _newActivityColor,
-              onMainColorChange: (color) => setState(() => _tempColor = color),
-              onColorChange: (color) => setState(() => _tempColor = color),
-            ),
-            actions: [
-              FlatButton(child: Text('CANCEL'), onPressed: Navigator.of(context).pop),
-              FlatButton(
-                child: Text('SUBMIT'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  setState(() {
-                    _newActivityColor = _tempColor;
-                  });
-                },
-              ),
-            ],
-          );
-        },
-      );
-    }
-
-    _openActivityDialog(BuildContext context) async {
+    _openActivityDialog(BuildContext context, [String activityName, Color activityColor]) async {
       return showDialog(
           context: context,
           builder: (context) {
-            _textFieldController.clear();
-
-            return AlertDialog(
-              title: Text('Activity details'),
-              content: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        autofocus: true,
-                        controller: _textFieldController,
-                        decoration: InputDecoration(hintText: 'Activity name'),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: _openColorPicker,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 16.0,
-                        child: Icon(Icons.color_lens, color: Colors.black),
-                      ),
-                    )
-                  ]),
-              actions: <Widget>[
-                FlatButton(
-                  child: new Text('CANCEL'),
-                  onPressed: Navigator.of(context).pop,
-                ),
-                FlatButton(
-                  child: new Text('SUBMIT'),
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                    setState(() {
-                      _newActivityName = _textFieldController.text;
-                    });
-                  },
-                ),
-              ],
-            );
+            return ActivityDialog(activityName: activityName, activityColor: activityColor);
           });
     }
 
     Widget slideRightBackground() {
       return Container(
-        color: Colors.green,
+        color: Colors.blue,
         child: Align(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               SizedBox(width: 20),
-              Icon(Icons.refresh, color: Colors.white),
+              Icon(Icons.edit, color: Colors.white),
               Text(
-                " Reset",
+                " Edit",
                 style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
                 textAlign: TextAlign.left,
               ),
@@ -178,7 +103,11 @@ class _ActivitiesGroupPageState extends State<ActivitiesGroupPage> {
                               });
                           return res;
                         } else {
-                          store.dispatch(ResetActivityAction(item));
+                          dynamic result = await _openActivityDialog(context, item.name, item.color);
+                          if (result != null) {
+                            store.dispatch(EditActivityAction(item, result['name'], 0, result['color']));
+                          }
+
                           return false;
                         }
                       },
@@ -189,15 +118,10 @@ class _ActivitiesGroupPageState extends State<ActivitiesGroupPage> {
                   builder: (context, ActivitiesGroup activitiesGroup) {
                     return FloatingActionButton(
                         onPressed: () async {
-                          if (!await _openActivityDialog(context)) {
-                            return;
+                          dynamic result = await _openActivityDialog(context);
+                          if (result != null) {
+                            store.dispatch(AddActivityAction(result['name'], 0, result['color']));
                           }
-
-                          if (_newActivityName == null || _newActivityName == '') {
-                            return;
-                          }
-
-                          store.dispatch(AddActivityAction(_newActivityName, 0, _newActivityColor));
                         },
                         child: Icon(Icons.alarm_add));
                   }));
---
+++ lib/modules/activity/widgets/activity-dialog.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+
+class ActivityDialog extends StatefulWidget {
+  final String activityName;
+  final Color activityColor;
+
+  const ActivityDialog({Key key, this.activityName, this.activityColor}) : super(key: key);
+
+  @override
+  _ActivityDialogState createState() => _ActivityDialogState();
+}
+
+class _ActivityDialogState extends State<ActivityDialog> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _textFieldController = TextEditingController(text: widget.activityName);
+    Color _tempColor;
+    Color _newActivityColor = widget.activityColor != null ? widget.activityColor : Colors.blue[100];
+
+    _openColorPicker() async {
+      return showDialog(
+          context: context,
+          child: AlertDialog(
+            contentPadding: const EdgeInsets.all(6.0),
+            title: Text("Pick the Activity color"),
+            content: MaterialColorPicker(
+              colors: materialColors,
+              selectedColor: _newActivityColor,
+              onMainColorChange: (color) => _tempColor = color,
+              onColorChange: (color) => _tempColor = color,
+            ),
+            actions: [
+              FlatButton(child: Text('CANCEL'), onPressed: Navigator.of(context).pop),
+              FlatButton(
+                child: Text('SUBMIT'),
+                onPressed: () {
+                  _newActivityColor = _tempColor;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ));
+    }
+
+    return AlertDialog(
+      title: Text('Activity details'),
+      content: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              child: TextField(
+                autofocus: true,
+                controller: _textFieldController,
+                decoration: InputDecoration(hintText: 'Activity name'),
+              ),
+            ),
+            GestureDetector(
+              onTap: _openColorPicker,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 16.0,
+                // Todo fix color not set in icon
+                child: Icon(Icons.color_lens, color: _newActivityColor),
+              ),
+            )
+          ]),
+      actions: <Widget>[
+        FlatButton(
+          child: new Text('CANCEL'),
+          onPressed: Navigator.of(context).pop,
+        ),
+        FlatButton(
+          child: new Text('SUBMIT'),
+          onPressed: () {
+            if (_textFieldController.text.isEmpty) {
+              return;
+            }
+
+            Navigator.of(context).pop({'name': _textFieldController.text, 'color': _newActivityColor});
+          },
+        ),
+      ],
+    );
+  }
+}
