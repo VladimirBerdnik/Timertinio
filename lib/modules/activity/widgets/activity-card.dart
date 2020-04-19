7d7b8c089cfa3d38eb9dfd4920b058f4c8ddfff2
Vladimir Berdnik <user.boba@gmail.com>
2020-04-19 16:03:48 +0600

    Get rid of unnecessary code in activity card

--- lib/modules/activity/widgets/activity-card.dart
+++ lib/modules/activity/widgets/activity-card.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -8,52 +6,33 @@ import 'package:timertinio/state/actions.dart';
 import 'package:timertinio/state/state.dart';
 
 class ActivityCard extends StatefulWidget {
-  ActivityCard(this.activity);
+  final Activity _activity;
 
-  final Activity activity;
+  ActivityCard(this._activity);
 
   @override
   State<StatefulWidget> createState() => ActivityCardState();
 }
 
 class ActivityCardState extends State<ActivityCard> {
-  Activity _activity;
-  Timer timer;
-
-  @override
-  void initState() {
-    _activity = widget.activity;
-    timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
-      // State is set to render fresh data in timer
-      this.setState(() {});
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, Store>(
         converter: (Store store) => store,
         builder: (context, Store store) {
           return Card(
-              color: _activity.color,
+              color: widget._activity.color,
               child: Column(
                 children: <Widget>[
                   ListTile(
-                    trailing: Icon(_activity.isStarted() ? Icons.pause : Icons.play_arrow),
-                    title: Text(_activity.name),
-                    subtitle: Text(_activity.duration.toString().split('.').first, textAlign: TextAlign.right),
+                    trailing: Icon(widget._activity.isStarted() ? Icons.pause : Icons.play_arrow),
+                    title: Text(widget._activity.name),
+                    subtitle: Text(widget._activity.duration.toString().split('.').first, textAlign: TextAlign.right),
                     onTap: () {
-                      if (_activity.isStarted()) {
-                        store.dispatch(StopActivityAction(_activity));
+                      if (widget._activity.isStarted()) {
+                        store.dispatch(StopActivityAction(widget._activity));
                       } else {
-                        store.dispatch(StartActivityAction(_activity));
+                        store.dispatch(StartActivityAction(widget._activity));
                       }
                     },
                   )
